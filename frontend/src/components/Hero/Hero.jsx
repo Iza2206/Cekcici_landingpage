2969a8b7921624b644cceafc7aca9b970b7d00f4
Iza2206 <izatunnisa06@gmail.com>
2024-10-31 16:20:36 +0700

    31102024

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -42,16 +42,16 @@ const Hero = () => {
         pauseOnFocus: true,
     };
   return (
-    <div>
-        <div className="w-full"> 
-            {/* Hero sction */}
-            <Slider {...settings}>
-                {
-                    HeroData.map((data) => (
+    <div className="container">
+        <div className="overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] hero-bg-color flex justify-center items-center"> 
+            <div className="container pb-8 sm:pb-0">
+                {/* Hero sction */}
+                <Slider {...settings}>
+                    {HeroData.map((data) => (
                         <div key={data.id}>
                             <div className="grid grid-cols-1 sm:grid-cols-2">
                                 {/* text content section */}
-                                <div>
+                                <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1">
                                     <h1>{data.subtitle}</h1>
                                     <h1>{data.title}</h1>
                                     <h1>{data.title2}</h1>
@@ -60,11 +60,16 @@ const Hero = () => {
                                     </div>
                                 </div>
                                 {/* img section */}
+                                <div>
+                                    <div>
+                                        <img src={data.img} alt="" className="w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgb(0,0,0.4)]" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    ))
-                }
-            </Slider>
+                    ))}
+                </Slider>
+            </div>
         </div>
     </div>
   )
